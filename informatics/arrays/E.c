7f3f81e44b263ec45679915820edeb662aaecb92
Ilya Klopov <ilyaklopov07@yandex.ru>
2025-10-01 19:00:51 +0300

    added files

---
+++ informatics/arrays/E.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+void read_array(int *size, int array[]) {
+    scanf("%d", size);
+    int i;
+    for (i = 0; i < *size; i++) {
+        scanf("%d", &array[i]);
+    }
+}
+
+
+int main() {
+    int size;
+    int wagons[1000];
+    read_array(&size, wagons);
+    int i;
+    int uncouplings = 0;
+    for (i = 0; i < size - 1; i++) {
+        if (wagons[i] + 1 != wagons[i + 1]) {
+            uncouplings++;
+        }
+    }
+    printf("%d", uncouplings);
+    return 0;
+}
