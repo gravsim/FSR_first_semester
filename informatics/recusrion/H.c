7abc5a8dfa7ada734073bb0d0edfa39297d95286
Ilya Klopov <ilyaklopov07@yandex.ru>
2025-11-02 17:37:54 +0300

    it prints shit

--- informatics/recusrion/H.c
+++ informatics/recusrion/H.c
@@ -2,13 +2,26 @@
 #include <stdlib.h>
 
 
-int get_sum(int N, int* scales) {
-    int i;
-    int sum = 0;
-    for (i = 0; i < N; i++) {
-        sum += scales[i];
+struct Scales {
+    int* left_weights;
+    int* right_weights;
+
+    int left_size;
+    int right_size;
+};
+
+
+void print_scales(int size, int* scales) {
+    if (size == 0) {
+        printf("0 ");
+        return;
     }
-    return sum;
+    int i = 0;
+    while (i < size) {
+        printf("%i ", scales[i]);
+        i++;
+    }
+    printf("\n");
 }
 
 
@@ -16,64 +29,73 @@ int put_on_sides(int side,
                 int N,
                 int i,
                 int* weights,
+                struct Scales* scales,
                 int left_sum,
                 int right_sum) {
+    printf("\nRight sum: %d Left sum: %d \n", right_sum, left_sum);
     if (left_sum == right_sum) {
         return 1;
     }
     if (i >= N) {
-        printf("ABOBA ");
+        // printf("Out of weights\n");
         return 0;
     }
-    
-    i++;
     switch (side) {
         case 0:
             left_sum += weights[i];
+            break;
         case 1:
-            right_sum += weights[i];  
+            right_sum += weights[i];
+            break;
     }
-
-
-    if (put_on_sides(0, N, i, weights, left_sum, right_sum)
-    || put_on_sides(1, N, i, weights, left_sum, right_sum)) {
-        printf("%i", weights[i]);
+    
+    printf("\n");
+    if (put_on_sides(0, N, i + 1, weights, scales, left_sum, right_sum)) {
+        scales->left_weights[scales->left_size] = weights[i];
+        (scales->left_size)++;
+        print_scales(scales->right_size, scales->right_weights);
+        print_scales(scales->left_size, scales->left_weights);
+        printf("\n");
+        return 1;
+    }
+    if (put_on_sides(1, N, i + 1, weights, scales, left_sum, right_sum)) {
+        scales->right_weights[scales->right_size] = weights[i];
+        (scales->right_size)++;
+        print_scales(scales->right_size, scales->right_weights);
+        print_scales(scales->left_size, scales->left_weights);
+        printf("\n");
         return 1;
     }
 }
 
 
-void print_scales(int N, int* scales) {
-    int i = 0;
-    if (scales[0] == 0) {
-        printf("0");
-        return;
-    }
-    while (i < N && scales[i] != 0) {
-        printf("%i", scales[i]);
-    }
-}
-
-
-int main() {
+int main(void) {
     int K_weight;
     int N;
     scanf("%i %i", &K_weight, &N);
-    // printf("%i    ", N);
+    // printf("N: %i\n", N);
     int* weights = (int*)calloc(N, sizeof(int));
-    int left_sum = 0;
-    int right_sum = 0;
+    struct Scales* scales = (struct Scales*)malloc(sizeof(struct Scales));
+    
+    scales->left_weights = (int*)calloc(N, sizeof(int));
+    scales->right_weights = (int*)calloc(N, sizeof(int));
+
+    scales->left_size = 0;
+    scales->right_size = 0;
     int i;
     for (i = 0; i < N; i++) {
         scanf("%i", &weights[i]);
     }
-
-    left_sum += K_weight;
-    if (put_on_sides(0, N, 0, weights, left_sum, right_sum) ||
-    put_on_sides(1, N, 0, weights, left_sum, right_sum)) {
+    if (put_on_sides(0, N, 0, weights, scales, K_weight, 0) ||
+    put_on_sides(1, N, 0, weights, scales, K_weight, 0)) {
+        print_scales(scales->right_size, scales->right_weights);
+        print_scales(scales->left_size, scales->left_weights);
     } else {
         printf("-1");
     }
     free(weights);
+    free(scales->left_weights);
+    free(scales->right_weights);
+    free(scales);
     return 0;
 }
