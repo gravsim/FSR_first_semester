3b8dbe4eb53ce74769399a35e6e3d7b3d2ee938c
Ilya Klopov <ilyaklopov07@yandex.ru>
2025-11-03 13:01:35 +0300

    all dads are free

--- informatics/recusrion/G.c
+++ informatics/recusrion/G.c
@@ -3,19 +3,20 @@
 
 
 typedef struct Child {
+    long id;
     long family_size;
     struct Child** children;
 } Child;
 
 
-int kill_family(Child* child, long* deleted) {
+int kill_children(Child* child, long* deleted) {
     if (!child) {
         return 0;
     }
     long i;
     (*deleted)++;
     for (i = 0; i < child->family_size; i++) {
-        kill_family(child->children[i], deleted);
+        kill_children(child->children[i], deleted);
     }
     return 0;
 }
@@ -23,26 +24,41 @@ int kill_family(Child* child, long* deleted) {
 
 int main(void) {
     long N;
-    long i;
+    long i, j;
     long number;
     long pointer;
     long k;
     long deleted = 0;
     scanf("%li", &N);
     Child** numbers = (Child**)calloc(N + 1, sizeof(Child*));
+    long* dad_ids = (long*)calloc(N + 1, sizeof(long));
     for (i = 1; i <= N; i++) {
+        scanf("%li %li", &number, &pointer);
         numbers[i] = (Child*)malloc(sizeof(Child));
         numbers[i]->children = (Child**)calloc(N + 1, sizeof(Child*));
         numbers[i]->family_size = 0;
+        numbers[i]->id = number;
+        dad_ids[i] = pointer;
     }
     for (i = 1; i <= N; i++) {
-        scanf("%li %li", &number, &pointer);
-        if (pointer > 0) {
-            numbers[pointer]->children[numbers[pointer]->family_size++] = numbers[number];
+        if (dad_ids[i] > 0) {
+            for (j = 1; j <= N; j++) {
+                if (dad_ids[i] == numbers[j]->id) {
+                    numbers[j]->children[numbers[j]->family_size++] = numbers[i];
+                }
+            }
         }
     }
     scanf("%li", &k);
-    kill_family(numbers[k], &deleted);
+    Child* target = NULL;
+    i = 1;
+    while (i <= N && !target) {
+        if (numbers[i]->id == k) {
+            target = numbers[i];
+        }
+        i++;
+    }
+    kill_children(target, &deleted);
     printf("%li", deleted);
     for (i = 1; i <= N; i++) {
         if (numbers[i]) {
@@ -51,5 +67,6 @@ int main(void) {
         }
     }
     free(numbers);
+    free(dad_ids);
     return 0;
 }
