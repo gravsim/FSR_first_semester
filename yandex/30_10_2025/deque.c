412fb447024c6af874d1d55f8954dc78fddda057
Ilya Klopov <ilyaklopov07@yandex.ru>
2025-11-19 17:09:09 +0300

    fuck 4th test!

--- yandex/30_10_2025/deque.c
+++ yandex/30_10_2025/deque.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 
 typedef struct Deque {
@@ -11,64 +10,91 @@ typedef struct Deque {
 } Deque;
 
 
+int norm_index(Deque* deque, int index) {
+    return (deque->capacity + index) % deque->capacity;
+}
+
+
+void print_deque(Deque* deque) {
+    int i;
+    printf("Deque: ");
+    for (i = deque->front; i < deque->back; i++) {
+        printf("%d ", deque->values[norm_index(deque, i)]);
+    }
+    printf("\n");
+}
+
+
+int is_full(Deque* deque) {
+    return deque->back - deque->front == deque->capacity;
+}
+
+
+int is_empty(Deque* deque) {
+    return deque->front == deque->back;
+}
+
+
 void expand(Deque* deque) {
+    int old_capacity = deque->capacity;
     deque->capacity *= 2;
     deque->values = (int*)realloc(deque->values, deque->capacity * sizeof(int));
+    int i;
+    for (i = 0; i < deque->front; i++) {
+        deque->values[i + old_capacity] = deque->values[i];
+    }
 }
 
 
 int push_front(Deque* deque, int* value) {
-    if (deque->front == deque->back) {
+    if (is_full(deque)) {
         expand(deque);
     }
-    deque->values[deque->front % deque->capacity] = *value;
     deque->front--;
+    deque->values[norm_index(deque, deque->front)] = *value;
     return 0;
 }
 
 
 int push_back(Deque* deque, int* value) {
-    if (deque->front == deque->back) {
+    if (is_full(deque)) {
         expand(deque);
     }
-    deque->values[deque->back % deque->capacity] = *value;
+    deque->values[norm_index(deque, deque->back)] = *value;
     deque->back++;
     return 0;
 }
 
 
 int pop_front(Deque* deque, int* value) {
-    *value = deque->values[deque->front++ - 1];
+    *value = deque->values[norm_index(deque, deque->front)];
+    deque->front++;
     return 0;
 }
 
 
 int pop_back(Deque* deque, int* value) {
-    *value = deque->values[deque->front-- - 1];
+    deque->back--;
+    *value = deque->values[norm_index(deque, deque->back)];
     return 0;
 }
 
 
 int front(Deque* deque, int* value) {
-    *value = deque->values[deque->front + 1];
+    *value = deque->values[norm_index(deque, deque->front)];
     return 0;
 }
 
 
 int back(Deque* deque, int* value) {
-    *value = deque->values[deque->back - 1];
+    *value = deque->values[norm_index(deque, deque->back - 1)];
     return 0;
-}
-
-
-int is_empty(Deque* deque) {
-    return abs(deque->back - deque->front) == 1;
 }
 
 
 int clear(Deque* deque) {
     deque->front = 0;
-    deque->back = 1;
+    deque->back = 0;
     return 0;
 }
 
@@ -76,7 +102,7 @@ int clear(Deque* deque) {
 int check_deque(Deque* deque) {
     int true_false = is_empty(deque);
     if (true_false) {
-        printf("\nDeque is empty");
+        printf("Deque is empty\n");
     }
     return !true_false;
 }
@@ -87,46 +113,46 @@ int main(void) {
     int value;
     Deque* deque = malloc(sizeof(Deque));
     deque->front = 0;
-    deque->back = 1;
-    deque->capacity = 100;
+    deque->back = 0;
+    deque->capacity = 10000000;
     deque->values = (int*)calloc(deque->capacity, sizeof(int));
     do {
         scanf("%i", &command);
         switch (command) {
             case 1:
-                scanf("%i", &value);
+                scanf(" %i", &value);
                 push_front(deque, &value);
                 break;
             case 2:
-                scanf("%i", &value);
+                scanf(" %i", &value);
                 push_back(deque, &value);
                 break;
             case 3:
                 if (check_deque(deque)) {
                     pop_front(deque, &value);
-                    printf("\n%i", value);
+                    printf("%i\n", value);
                 }
                 break;
             case 4:
                 if (check_deque(deque)) {
                     pop_back(deque, &value);
-                    printf("\n%i", value);
+                    printf("%i\n", value);
                 }
                 break;
             case 5:
                 if (check_deque(deque)) {
                     front(deque, &value);
-                    printf("\n%i", value);
+                    printf("%i\n", value);
                 }
                 break;
             case 6:
                 if (check_deque(deque)) {
                     back(deque, &value);
-                    printf("\n%i", value);
+                    printf("%i\n", value);
                 }
                 break;
             case 7:
-                printf("\n%i", is_empty(deque));
+                printf("%i\n", is_empty(deque));
                 break;
             case 8:
                 clear(deque);
@@ -134,6 +160,7 @@ int main(void) {
             default:
                 break;
         }
+        // print_deque(deque);
     } while (command != 0);
     free(deque->values);
     free(deque);
