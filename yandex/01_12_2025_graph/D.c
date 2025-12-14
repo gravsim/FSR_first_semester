79b6793cd0e84723bea070ceb15bc1900952ea46
Ilya Klopov <ilyaklopov07@yandex.ru>
2025-12-14 18:22:42 +0300

    d.c graph done

--- yandex/01_12_2025_graph/D.c
+++ yandex/01_12_2025_graph/D.c
@@ -28,31 +28,76 @@ void set_bit(unsigned long long* number, int bit_index) {
 }
 
 
+int DFS_recursive(unsigned long long** connections,
+                    int* colors,
+                    int current,
+                    int N) {
+    if (!connections || !colors) {
+        return -1;
+    }
+    colors[current] = 1;
+    int i;
+    for (i = 0; i < N; i++) {
+        if (check_bit(connections[current][i / CHUNK_SIZE], i)
+            && check_bit(connections[current][i / CHUNK_SIZE], i) != check_bit(connections[i][current / CHUNK_SIZE], current)) {
+            // Второе условие проверяет, что нет прохода по одному
+            // и тому же ребру, чтобы цикл был простой
+            if (colors[i] == 1 ||
+                (colors[i] == 0 && DFS_recursive(connections, colors, i, N))) {
+                return 1;
+            }
+        }
+    }
+    colors[current] = 2;
+    return 0;
+}
+
+
+int check_cycles(unsigned long long** connections, int V) {
+    int i;
+    int* colors = calloc(V, sizeof(int));
+    for (i = 0; i < V; i++) {
+        if (colors[i] == 0
+            && DFS_recursive(connections, colors, i, V)) {
+            return 1;
+        }
+    }
+    free(colors);
+    return 0;
+}
+
+
 int main(void) {
+    /*
+     Сложность в том, что нужно определить, есть ли простой цикл, а
+     значит он не может проходит по одному и тому же ребру дважды.
+    */
     int V;
     int E;
     scanf("%d %d", &V, &E);
     int numbers = (V + CHUNK_SIZE - 1) / CHUNK_SIZE;
-    unsigned long long** connections = allocate_matrix(V, numbers);
-    int vertex;
     int i;
     int j;
-    int answer = 1;
+    unsigned long long** connections = allocate_matrix(V, numbers);
     int* borders = calloc(V + 1, sizeof(int));
-    int* vertices = calloc(V + 1, sizeof(int));
+    int* vertices = calloc(E, sizeof(int));
     int* weights = calloc(E, sizeof(int));
+    // Массив weights бесполезен
     for (i = 0; i < V + 1; i++) {
         scanf("%d", &borders[i]);
     }
+    for (i = 0; i < E; i++) {
+        scanf("%d", &vertices[i]);
+    }
+    for (i = 0; i < E; i++) {
+        scanf("%d", &weights[i]);
+    }
     for (i = 0; i < V; i++) {
         for (j = borders[i]; j < borders[i + 1]; j++) {
-            scanf("%d", &vertex);
-            set_bit(&connections[i][vertex / CHUNK_SIZE], vertex);
-            set_bit(&connections[vertex][i / CHUNK_SIZE], i);
+            set_bit(&connections[i][vertices[j] / CHUNK_SIZE], vertices[j]);
         }
     }
-
-    if (answer) {
+    if (check_cycles(connections, V)) {
         printf("YES");
     } else {
         printf("NO");
@@ -61,5 +106,8 @@ int main(void) {
         free(connections[i]);
     }
     free(connections);
+    free(borders);
+    free(vertices);
+    free(weights);
     return 0;
 }
