a03ee5ddb4d224e2724f05dc7839a9ba3546c449
Ilya Klopov <ilyaklopov07@yandex.ru>
2026-02-15 14:31:25 +0300

    #00000016

--- semester2/informatics/2026-02-25 DSU_MST/C.c
+++ semester2/informatics/2026-02-25 DSU_MST/C.c
@@ -1,36 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-#define MAX_WEIGHT 30001
 
+
+int** set_adjacency_matrix(int N) {
+    int i;
+    int** adjacency_matrix = calloc(N, sizeof(int*));
+    for (i = 0; i < N; i++) {
+        adjacency_matrix[i] = calloc(N, sizeof(int));
+    }
+    return adjacency_matrix;
+}
 
 
 typedef struct DSU_node {
     struct DSU_node* parent;
-    int value;
+    int sum;
     int rang;
 } DSU_node;
 
 
-DSU_node* make_set(int value) {
+DSU_node* make_set(int sum) {
     DSU_node* tmp = malloc(sizeof(DSU_node));
-    tmp->value = value;
+    tmp->sum = sum;
     tmp->parent = tmp;
     tmp->rang = 1;
     return tmp;
-}
-
-void swap(int* a, int* b) {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
-
-
-void swap_nodes(DSU_node* node1, DSU_node* node2) {
-    DSU_node tmp = *node1;
-    *node1 = *node2;
-    *node2 = tmp;
 }
 
 
@@ -43,74 +39,191 @@ DSU_node* find_set(DSU_node* node) {
 
 
 DSU_node* union_set(DSU_node* node1, DSU_node* node2) {
+    if (!node1 || !node2) {
+        return NULL;
+    }
     node1 = find_set(node1);
     node2 = find_set(node2);
+    if (node1 == node2) {
+        return node1;
+    }
     if (node1->rang < node2->rang) {
-        swap_nodes(node1, node2);
+        DSU_node* tmp = node1;
+        node1 = node2;
+        node2 = tmp;
     }
     node2->parent = node1;
     if (node1->rang == node2->rang) {
         node1->rang++;
     }
+    node1->sum += node2->sum;
     return node1;
 }
 
 
-int BFS(int** adjacency_matrix, int n, int source) {
-    if (!adjacency_matrix) {
-        return -1;
-    }
-    int* visited = calloc(n, sizeof(int));
-    int* queue = calloc(n, sizeof(int));
-    int front = 0;
-    int back = 0;
-    int weight = 0;
-    int current;
-    int i;
-    visited[source] = 1;
-    queue[back++] = source;
-    while (front < back) {
-        current = queue[front];
-        for (i = 0; i < n; i++) {
-            if (adjacency_matrix[current][i] && !visited[i]) {
-                visited[i] = 1;
-                queue[back++] = i;
-                weight += adjacency_matrix[current][i];
-            }
-        }
-        front++;
-    }
-    free(visited);
-    free(queue);
-    return weight;
+typedef struct Heap {
+    int size;
+    int capacity;
+    double* values;
+} Heap;
+
+
+int swap(double* a, double* b) {
+    double tmp = *a;
+    *a = *b;
+    *b = tmp;
+    return 0;
 }
 
 
+int is_full(Heap* heap) {
+    return heap->size == heap->capacity;
+}
 
-int main(void) {
-    int n;
-    int m;
+
+int expand(Heap* heap) {
+    if (!heap) {
+        return -1;
+    }
+    heap->capacity *= 2;
+    heap->values = (double*)realloc(heap->values, heap->capacity * sizeof(double));
+    return 1;
+}
+
+
+int sift_up(Heap* heap, int index) {
+    if (!heap) {
+        return -1;
+    }
+    while (index > 0 && heap->values[index] < heap->values[(index - 1) / 2]) {
+        swap(&heap->values[index], &heap->values[(index - 1) / 2]);
+        index = (index - 1) / 2;
+    }
+    return 1;
+}
+
+
+int sift_down(Heap* heap, int index) {
+    int max_index = index;
+    if (2 * index + 2 < heap->size && heap->values[2 * index + 2] < heap->values[max_index]) {
+        max_index = 2 * index + 2;
+    }
+    if (2 * index + 1 < heap->size && heap->values[2 * index + 1] <= heap->values[max_index]) {
+        max_index = 2 * index + 1;
+    }
+    /*
+        Тут знак >=, т.к. при равенстве элементов по условию задачи,
+        нужно выбирать левого сына, который имеет индекс 2 * index + 1.
+    */
+    if (max_index != index) {
+        swap(&heap->values[index], &heap->values[max_index]);
+        return sift_down(heap, max_index);
+    }
+    return max_index;
+}
+
+
+int push(Heap* heap, int value) {
+    if (!heap) {
+        return -1;
+    }
+    if (is_full(heap)) {
+        expand(heap);
+    }
+    heap->values[heap->size] = value;
+    sift_up(heap, heap->size);
+    heap->size++;
+    return 1;
+}
+
+
+int print_heap(Heap* heap) {
+    if (!heap) {
+        return -1;
+    }
     int i;
-    int command;
-    int** adjacency_matrix = set_adjacency_matrix(n);
+    for (i = 0; i < heap->size; i++) {
+        printf("%lf ", heap->values[i]);
+    }
+    return 1;
+}
+
+
+int decrease(Heap* heap, int index, int value) {
+    heap->values[index] -= value;
+    return sift_down(heap, index) + 1;
+}
+
+
+int init_heap(Heap** heap) {
+    if (!heap) {
+        return -1;
+    }
+    *heap = malloc(sizeof(Heap));
+    (*heap)->size = 0;
+    (*heap)->capacity = 5;
+    (*heap)->values = (double*)calloc((*heap)->capacity, sizeof(double));
+    return 1;
+}
+
+
+int forms_cycle(int* edges, int new_adge) {
+
+
+}
+
+
+int flatten_indices(int N, int i, int j) {
+    return i * (N - 1) - i * (i + 1) / 2 + j;
+}
+
+
+int Kruskal(Heap* heap, int N, DSU_node** nodes) {
+    int edges_amount = 0;
+    int i;
     int x;
     int y;
-    int w;
-    scanf("%d %d", &n, &m);
-    for (i = 0; i < m; i++) {
-        scanf("%d", &command);
-        switch (command) {
-            case 1:
-                scanf("%d %d %d", &x, &y, &w);
-                DSU_node* new_node = malloc(sizeof(DSU_node));
-                new_node->value = w;
+    while (edges_amount < N - 1) {
+        if (find_set(nodes[x]) != find_set(nodes[y])) {
+            union_set(nodes[x], nodes[y]);
+            edges_amount++;
+        }
 
-            case 2:
-                scanf("%d", &x);
+    }
+    return find_set(nodes[edges_amount])->sum;
+}
 
+
+int main(void) {
+    int N;
+    int M;
+    int i;
+    int j;
+    scanf("%d", &N);
+    Heap* heap;
+    init_heap(&heap);
+    int** adjacency_matrix = set_adjacency_matrix(N);
+    int** positions = calloc(N, sizeof(int*));
+    DSU_node** nodes = calloc(N, sizeof(DSU_node*));
+    double* edges = calloc(N * (N - 1) / 2, sizeof(int));
+    for (i = 0; i < N; i++) {
+        positions[i] = calloc(2, sizeof(int));
+        nodes[i] = make_set(0);
+    }
+    for (i = 0; i < N; i++) {
+        scanf("%d %d", positions[i], positions[i] + 1);
+    }
+    for (i = 0; i < N; i++) {
+        for (j = i + 1; j < N; j++) {
+            push(heap, sqrt(positions[i][0] ^ 2 - positions[j][1] ^ 2));
         }
     }
+    printf("%d", Kruskal(edges, N, nodes));
+    scanf("%d", &M);
+    for (i = 0; i < M; i++) {
+        scanf("%d %d", positions[i], positions[i] + 1);
+    }
 
-    printf("%d", get_MST_weight_Prim(N, adjacency_matrix));
+
     return 0;
 }
