2ec8fa333d3a2ec47bf05ffa1910cd6606095864
manki <ilyaklopov07@yandex.ru>
2026-03-04 22:04:50 +0300

    #00000046

--- semester2/yandex/2026-02-19/A.c
+++ semester2/yandex/2026-02-19/A.c
@@ -1,124 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-
-#define MAX_WEIGHT 100
 
 
-int swap_int_pointers(int** a, int** b) {
-    if (!a || !b) {
-        return -1;
+int print_way(int v, int i, int n, int* previous) {
+    if (i < 0 || i >= n) {
+        return 0;
     }
-    int* tmp = *a;
-    *a = *b;
-    *b = tmp;
+    if (i == v) {
+        printf("%d ", i);
+        return 1;
+    }
+    if (previous[i] == -1) {
+        return 0;
+    }
+    if (!print_way(v, previous[i], n, previous)) {
+        return 0;
+    }
+    printf("%d ", i);
     return 1;
 }
-
-int swap_int(int* a, int* b) {
-    if (!a || !b) {
-        return -1;
-    }
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
-    return 1;
-}
-
-
-int get_minimal_weight(int* distances, int* visited, int V) {
-    if (!distances || !visited) {
-        return -1;
-    }
-    int i = 0;
-    int minimum = MAX_WEIGHT;
-    int cheap_index = -1;
-    for (i = 0; i < V; i++) {
-        if (!visited[i] && distances[i] < minimum) {
-            minimum = distances[i];
-            cheap_index = i;
-        }
-    }
-    return cheap_index;
-}
-
-
-int** set_adjacency_matrix(int V, int M, int** edges) {
-    int i;
-    int vertex1;
-    int vertex2;
-    int weight;
-    int** adjacency_matrix = calloc(V, sizeof(int*));
-    for (i = 0; i < V; i++) {
-        adjacency_matrix[i] = calloc(V, sizeof(int));
-    }
-    for (i = 0; i < M; i++) {
-        vertex1 = edges[i][0];
-        vertex2 = edges[i][1];
-        weight = edges[i][2];
-
-        adjacency_matrix[vertex1][vertex2] = weight;
-        adjacency_matrix[vertex2][vertex1] = weight;
-    }
-    return adjacency_matrix;
-}
-
-
-int free_adjacency_matrix(int** adjacency_matrix, int V) {
-    if (!adjacency_matrix) {
-        return -1;
-    }
-    int i;
-    for (i = 0; i < V; i++) {
-        free(adjacency_matrix[i]);
-    }
-    free(adjacency_matrix);
-    return 1;
-}
-
-
-
-void quick_sort(int** main_array, int sort_index, int size, int down, int up) {
-    if (down >= up) {
-        return;
-    }
-    int pivot = main_array[(up + down) / 2][sort_index];
-    int left = down;
-    int right = up;
-    while (left <= right) {
-        while (main_array[left][sort_index] < pivot) {
-            left++;
-        }
-        while (pivot < main_array[right][sort_index]) {
-            right--;
-        }
-        if (left <= right) {
-            swap_int_pointers(main_array + left, main_array + right);
-            left++;
-            right--;
-        }
-    }
-    quick_sort(main_array, sort_index, size, down, right);
-    quick_sort(main_array, sort_index, size, left, up);
-}
-
-
-
 
 
 int main(void) {
     int v;
-    int n;
+    int n = 10000;
     int command;
     int i;
     int* previous = calloc(n, sizeof(int));
     scanf("%d", &command);
-    for (i = 0; i < n; i++) {
-        scanf("%d", previous + i);
-    }
+    int to;
     while (command != 3) {
-        
+        switch (command) {
+            case 1:
+                scanf("%d %d", &v, &n);
+                for (i = 0; i < n; i++) {
+                    scanf("%d", previous + i);
+                }
+                break;
+            case 2:
+                scanf("%d", &to);
+                if (previous[to] == -1 || !print_way(v, to, n, previous)) {
+                    printf("-1");
+                }
+                break;
+            default:
+                break;
+        }
+        scanf("%d", &command);
     }
-    
     return 0;
 }
