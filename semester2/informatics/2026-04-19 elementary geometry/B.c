fe3c7ee11e844faef4ee17cef9e9a71b1a3e9612
manki <ilyaklopov07@yandex.ru>
2026-03-22 14:05:21 +0300

    #00000089

--- semester2/informatics/2026-04-19 elementary geometry/B.c
+++ semester2/informatics/2026-04-19 elementary geometry/B.c
@@ -17,8 +17,26 @@ double distance(double vector1[2], double vector2[2]) {
 }
 
 
-int double_equal(double a, double b) {
+double double_equal(double a, double b) {
     return fabs(a - b) <= EPSILON;
+}
+
+
+void swap(double* a, double* b) {
+    double* tmp = a;
+    *a = *b;
+    *b = *tmp;
+}
+
+
+void check_points(double x1, double x2, double* max, double* min) {
+    if (x1 < x2) {
+        *max = x2;
+        *min = x1;
+    } else {
+        *max = x1;
+        *min = x2;
+    }
 }
 
 
@@ -26,11 +44,14 @@ int main(void) {
     double* A = malloc(2 * sizeof(double));
     double* B = malloc(2 * sizeof(double));
     double* point = malloc(2 * sizeof(double));
-    scanf("%lf %lf %lf %lf %lf %lf", A, A + 1, B, B + 1, point, point + 1);
+    scanf("%lf %lf %lf %lf %lf %lf", point, point + 1, A, A + 1, B, B + 1);
     if (double_equal(distance(A, point) + distance(B, point), distance(A, B))) {
         printf("YES");
     } else {
         printf("NO");
     }
+    free(A);
+    free(B);
+    free(point);
     return 0;
 }
