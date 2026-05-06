38d96a30c5401fd7ea3916242509dffb949fcd1d
manki <ilyaklopov07@yandex.ru>
2026-05-06 20:51:29 +0300

    #00000198

--- semester2/yandex/2026-04-23_trie/B.c
+++ semester2/yandex/2026-04-23_trie/B.c
@@ -2,158 +2,136 @@
 #include <stdlib.h>
 
 
-#define MAX_LENGTH 1000000
+#define MAX_LENGTH 10000
+#define ALPHABET_SIZE 6300
+
 
 struct TrieNode {
-    struct TrieNode* next;
-    struct TrieNode* child;
-    char symbol;
+    struct TrieNode** children;
     int end_of_word;
 } typedef TrieNode;
 
 
-TrieNode** Trie_search_node(TrieNode** current, int value) {
-    while (*current && (*current)->value != value) {
-        if ((*current)->value > value) {
-            current = &(*current)->right;
-        } else {
-            current = &(*current)->left;
-        }
+TrieNode** Trie_search_node(TrieNode** current, char* word, int length) {
+    int i = 0;
+    int index = word[i] - 'a';
+    while (i < length && *current && (*current)->children[index]) {
+        index = word[i] - 'a';
+        current = &(*current)->children[index];
+        i++;
+    }
+    if (i < length) {
+        return 0;
     }
     return current;
 }
 
 
-TrieNode** Trie_find_min(TrieNode** node) {
-    if (!*node) {
-        return NULL;
-    }
-    while ((*node)->right) {
-        node = &(*node)->right;
-    }
-    return node;
-}
-
-
 int Trie_delete_node(TrieNode** node) {
-    if (!node || !*node) {
-        return 0;
-    }
-    TrieNode* tmp = *node;
-    if (!(*node)->right && !(*node)->left) {
-        free(tmp);
-        *node = NULL;
-        return 1;
-    }
-    if (!(*node)->right) {
-        *node = (*node)->left;
-        free(tmp);
-        return 1;
-    }
-    if (!(*node)->left) {
-        *node = (*node)->right;
-        free(tmp);
-        return 1;
-    }
-    TrieNode** descendant = Trie_find_min(&(*node)->left);
-    (*node)->value = (*descendant)->value;
-    return Trie_delete_node(descendant);
-}
-
-
-TrieNode* Trie_new_node(char value) {
-    TrieNode* new_node = malloc(sizeof(TrieNode));
-    new_node->child = NULL;
-    new_node->next = NULL;
-    new_node->end_of_word = 0;
-    new_node->symbol = value;
-    return new_node;
-}
-
-
-TrieNode* Trie_push(TrieNode* root, int value) {
-    if (!root) {
-        return Trie_new_node(value);
-    }
-    if (root->value > value) {
-        root->right = Trie_push(root->right, value);
-    } else {
-        root->left = Trie_push(root->left, value);
-    }
-    return root;
-}
-
-
-int Trie_free_node(TrieNode** leaf) {
-    if ((*leaf)->left) {
-        Trie_free_node(&(*leaf)->left);
-    }
-    if ((*leaf)->right) {
-        Trie_free_node(&(*leaf)->right);
-    }
-    free(*leaf);
-    *leaf = NULL;
+    (*node)->end_of_word = 0;
     return 1;
 }
 
 
-int Trie_free_root(TrieNode** root) {
-    if (!root || !*root) {
-        return -1;
+TrieNode* Trie_new_node() {
+    TrieNode* new_node = malloc(sizeof(TrieNode));
+    new_node->children = calloc(ALPHABET_SIZE, sizeof(TrieNode*));
+    new_node->end_of_word = 0;
+    return new_node;
+}
+
+
+TrieNode* Trie_push(TrieNode* root, char* word, int length) {
+    if (!root) {
+        return Trie_new_node();
     }
-    if ((*root)->left) {
-        Trie_free_node(&(*root)->left);
+    TrieNode* current = root;
+    int i;
+    int index;
+    for (i = 0; i < length; i++) {
+        index = word[i] - 'a';
+        if (!current->children[index]) {
+            current->children[index] = Trie_new_node();
+        }
+        current = current->children[index];
     }
-    if ((*root)->right) {
-        Trie_free_node(&(*root)->right);
+    current->end_of_word = 1;
+    return root;
+}
+
+
+int Trie_free_node(TrieNode** node) {
+    int i;
+    for (i = 0; i < ALPHABET_SIZE; i++) {
+        if ((*node)->children[i]) {
+            Trie_free_node(&(*node)->children[i]);
+        }
     }
-    free(*root);
-    *root = NULL;
+    free(*node);
+    *node = NULL;
+    return 1;
+}
+
+
+int Trie_print(TrieNode** node) {
+    int i;
+    for (i = 0; i < ALPHABET_SIZE; i++) {
+        if ((*node)->children[i]) {
+            printf("%c", 'a' + i);
+            if ((*node)->end_of_word) {
+                printf("\n");
+            } else {
+                Trie_print(&(*node)->children[i]);
+            }
+        }
+    }
     return 1;
 }
 
 
 int main(void) {
     int command;
-    int value;
     TrieNode* root = NULL;
     int length = 0;
     char string[MAX_LENGTH];
+    TrieNode** found;
     do {
         scanf("%d", &command);
         switch (command) {
             case 1:
+                length = 0;
                 while (scanf("%c", string + length) != EOF && string[length] != '\n') {
                     length++;
                 }
-                root = Trie_push(root, value);
+                root = Trie_push(root, string, length);
                 printf("1\n");
                 break;
             case 2:
+                length = 0;
                 while (scanf("%c", string + length) != EOF && string[length] != '\n') {
                     length++;
                 }
-                if (*Trie_search_node(&root, value)) {
+                if (Trie_search_node(&root, string, length)) {
                     printf("1\n");
                 } else {
                     printf("0\n");
                 }
                 break;
             case 3:
+                length = 0;
                 while (scanf("%c", string + length) != EOF && string[length] != '\n') {
                     length++;
                 }
-                TrieNode** found = Trie_search_node(&root, value);
+                found = Trie_search_node(&root, string, length);
                 printf("%d\n", Trie_delete_node(found));
                 break;
             case 4:
-                TrieNode** found = Trie_search_node(&root, value);
-                printf("%d\n", Trie_delete_node(found));
+                Trie_print(&root);
                 break;
             default:
                 break;
         }
     } while (command != 0);
-    Trie_free_root(&root);
+    Trie_free_node(&root);
     return 0;
 }
