5ba1acfabc98ff2d5bc60f9ebf5a7e3af5c48a6e
manki <ilyaklopov07@yandex.ru>
2026-05-06 15:07:47 +0300

    #00000184

--- semester2/informatics/2026-05-20_1_strings/B.c
+++ semester2/informatics/2026-05-20_1_strings/B.c
@@ -6,12 +6,24 @@
 
 int smaller(char* a, char* b, int len_a, int len_b) {
     int i = 0;
-    int min_len = min(len_a, len_b);
-    while (i < min_len && a[i] == b[i]) {
+    int union_len = len_a + len_b;
+    char char_a;
+    char char_b;
+    do {
+        if (i < len_a) {
+            char_a = a[i];
+        } else {
+            char_a = b[i % len_a];
+        }
+        if (i < len_b) {
+            char_b = b[i];
+        } else {
+            char_b = a[i % len_b];
+        }
         i++;
-    }
-    if (i < min_len) {
-        if (a[i] < b[i]) {
+    } while (i < union_len && char_a == char_b);
+    if (i < union_len) {
+        if (char_a < char_b) {
             return 1;
         }
         return 0;
@@ -48,13 +60,14 @@ void quick_sort(char** array, int* lengths, int size, int down, int up) {
     }
     int pivot_index = (up + down) / 2;
     char* pivot = array[pivot_index];
+    int pivot_length = lengths[pivot_index];
     int left = down;
     int right = up;
     while (left <= right) {
-        while (smaller(array[left], pivot, lengths[left], lengths[pivot_index])) {
+        while (left <= up && smaller(array[left], pivot, lengths[left], pivot_length)) {
             left++;
         }
-        while (smaller(pivot, array[right], lengths[left], lengths[pivot_index])) {
+        while (right >= down && smaller(pivot, array[right], pivot_length, lengths[right])) {
             right--;
         }
         if (left <= right) {
@@ -70,7 +83,7 @@ void quick_sort(char** array, int* lengths, int size, int down, int up) {
 
 
 int main(void) {
-    int max_length = 100;
+    int max_length = 101;
     int amount = 0;
     int i;
     char** strings = calloc(max_length, sizeof(char*));
@@ -80,13 +93,18 @@ int main(void) {
     int* lengths = calloc(max_length, sizeof(int));
     while (scanf("%c", strings[amount] + lengths[amount]) != EOF) {
         if (strings[amount][lengths[amount]] == '\n') {
+            strings[amount][lengths[amount]] = '\0';
             amount++;
         } else {
             lengths[amount]++;
         }
     }
+    if (lengths[amount] > 0) {
+        strings[amount][lengths[amount]] = '\0';
+        amount++;
+    }
     quick_sort(strings, lengths, amount, 0, amount - 1);
-    for (i = 0; i < amount; i++) {
+    for (i = amount - 1; i >= 0; i--) {
         printf("%s", strings[i]);
     }
     free(lengths);
