4b33a293a9d6a1f669f3b433127d90198f1179b2
manki <ilyaklopov07@yandex.ru>
2026-05-13 19:45:13 +0300

    #00000213

--- semester2/yandex/2026-05-11_hash_table/A_open_addressation.c
+++ semester2/yandex/2026-05-11_hash_table/A_open_addressation.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define A 2.718281828459045235360
+
 #define TABLE_SIZE 10
-#define NO 10
-#define DEL 10
+#define NO (-10)
+#define DEL (-20)
 
 
 
 typedef struct Node
 {
-    int key;
+    unsigned int key;
     double value;
 } Node;
 
@@ -26,36 +26,42 @@ double fraction(double a) {
 }
 
 
-unsigned int Hash_function(unsigned int key) {
-    return (unsigned int)(fraction(key * A) * TABLE_SIZE) % TABLE_SIZE;
+unsigned int Hash_function(unsigned int key, unsigned int i) {
+    return (key + i) % TABLE_SIZE;
 }
 
 
-
-int Table_search(Hash_table* Hash_table, int key, double* value) {
-    int index = Hash_function(key);
+int Table_search(Hash_table* hash_table, unsigned int key, double* value) {
     int i = 0;
-    while (i < TABLE_SIZE && Hash_table->values[index]->key != key) {
-        index = Hash_function(index);
+    unsigned int index = Hash_function(key, i);
+    while (i < TABLE_SIZE && hash_table->values[index] && hash_table->values[index]->key != key) {
         i++;
+        index = Hash_function(key, i);
     }
-    if (i < TABLE_SIZE && Hash_table->values[index]->key == key) {
-        Node* found = Hash_table->values[Hash_function(index)];
+    Node* found = hash_table->values[index];
+    if (i < TABLE_SIZE && found && found->key == key) {
         *value = found->value;
+
         return 1;
     }
     return 0;
 }
 
 
-int Table_push(Hash_table* Hash_table, int key, double value) {
-    double val;
-    if (Table_search(Hash_table, key, &val)) {
+int Table_push(Hash_table* hash_table, unsigned int key, double value) {
+    double val = 0;
+    if (Table_search(hash_table, key, &val)) {
         return 2;
     }
-    int index = Hash_function(key);
-    while (Hash_table->values[index]->key != NO) {
-        index = Hash_function(index);
+    int i = 0;
+    unsigned int index = Hash_function(key, i);
+
+    while (i < TABLE_SIZE && hash_table->values[index] && hash_table->values[index]->key != NO) {
+        i++;
+        index = Hash_function(key, i);
+    }
+    if (i >= TABLE_SIZE) {
+        return 3;
     }
     Node* new_node = malloc(sizeof(Node));
     if (!new_node) {
@@ -63,31 +69,37 @@ int Table_push(Hash_table* Hash_table, int key, double value) {
     }
     new_node->value = value;
     new_node->key = key;
-    Hash_table->values[index] = new_node;
+    hash_table->values[index] = new_node;
     return 0;
 }
 
 
-int Table_delete_value(Hash_table* Hash_table, int key) {
-    int index = Hash_function(key);
+int Table_delete_value(Hash_table* hash_table, unsigned int key) {
     int i = 0;
-    while (i < TABLE_SIZE && Hash_table->values[index]->key != key) {
-        index = Hash_function(index);
+    unsigned int index = Hash_function(key, i);
+    while (i < TABLE_SIZE && hash_table->values[index] && hash_table->values[index]->key != key) {
         i++;
+        index = Hash_function(key, i);
     }
-    if (i < TABLE_SIZE && Hash_table->values[index]->key == key) {
-        Node* found = Hash_table->values[Hash_function(index)];
-        *value = found->value;
+    if (i < TABLE_SIZE && hash_table->values[index] && hash_table->values[index]->key == key) {
+        Node* found = hash_table->values[Hash_function(key, i)];
+        found->key = DEL;
         return 1;
     }
     return 0;
 }
 
 
-int Table_clear(Hash_table* Hash_table) {
+int Table_clear(Hash_table* hash_table) {
+    if (hash_table == NULL || hash_table->values == NULL) {
+        return 1;
+    }
     int i;
     for (i = 0; i < TABLE_SIZE; i++) {
-        free(Hash_table->values[i]);
+
+        if (hash_table->values[i]) {
+            hash_table->values[i]->key = NO;
+        }
     }
     return 0;
 }
@@ -98,19 +110,19 @@ int main(void) {
     int key;
     double value;
     int status = 0;
-    Hash_table* Hash_table = malloc(sizeof(Hash_table));
-    Hash_table->max_size = TABLE_SIZE;
-    Hash_table->values = calloc(Hash_table->max_size, sizeof(Node*));
+    Hash_table* hash_table = malloc(sizeof(Hash_table));
+    hash_table->max_size = TABLE_SIZE;
+    hash_table->values = calloc(hash_table->max_size, sizeof(Node*));
     do {
         scanf("%d", &command);
         switch (command) {
             case 1:
                 scanf(" %d %lf", &key, &value);
-                printf("%d\n", Table_push(Hash_table, key, value));
+                printf("%d\n", Table_push(hash_table, key, value));
                 break;
             case 2:
                 scanf(" %d", &key);
-                if (Table_search(Hash_table, key, &value)) {
+                if (Table_search(hash_table, key, &value)) {
                     printf("%lf\n", value);
                 } else {
                     printf("Not found\n");
@@ -118,21 +130,21 @@ int main(void) {
                 break;
             case 3:
                 scanf(" %d", &key);
-                if (Table_delete_value(Hash_table, key)) {
+                if (Table_delete_value(hash_table, key)) {
                     printf("0\n");
                 } else {
                     printf("Not found\n");
                 }
                 break;
             case 4:
-                Table_clear(Hash_table);
+                Table_clear(hash_table);
                 printf("%d\n", status);
                 break;
             default:
                 break;
         }
     } while (command != 0);
-    free(Hash_table->values);
-    free(Hash_table);
+    free(hash_table->values);
+    free(hash_table);
     return 0;
 }
