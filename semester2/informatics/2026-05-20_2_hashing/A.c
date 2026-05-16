c410160ab49a729f8a53841ae7b90d96f1e8f4b8
manki <ilyaklopov07@yandex.ru>
2026-05-16 23:18:47 +0300

    #00000219

--- semester2/informatics/2026-05-20_2_hashing/A.c
+++ semester2/informatics/2026-05-20_2_hashing/A.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 
+#define A 2.718281828459045235360
+#define TABLE_SIZE 10000
+#define FUNCTION_TYPE 0
 #define MAX_LENGTH 11
-#define ALPHABET_SIZE 26
-
-
-struct TrieNode {
-    struct TrieNode* children[ALPHABET_SIZE];
-    int end_of_word;
-} typedef TrieNode;
 
 
 int get_index(char symbol) {
@@ -17,74 +14,155 @@ int get_index(char symbol) {
 }
 
 
-char get_char(int index) {
-    return 'a' + index;
-}
-
-
-TrieNode** Trie_search_node(TrieNode** current, char* word, int length) {
+int string_equal(char* string1, char* string2) {
+    if (string1 == NULL || string2 == NULL) {
+        return -1;
+    }
     int i = 0;
-    int index;
-    while (i < length && *current) {
-        index = get_index(word[i]);
-        current = &(*current)->children[index];
+    while (string1[i] && string2[i] && string1[i] == string2[i]) {
         i++;
     }
-    if (i < length) {
+    if (i == MAX_LENGTH) {
+        return 1;
+    }
+    return 0;
+}
+
+
+typedef struct Node
+{
+    char* string;
+    int length;
+    struct Node* next;
+} Node;
+
+
+Node* List_search(Node* head, char* string) {
+    if (head == NULL) {
         return NULL;
     }
-    return current;
+    while (head->next && !string_equal(head->string, string)) {
+        head = head->next;
+    }
+    if (string_equal(head->string, string)) {
+        return head;
+    }
+    return NULL;
 }
 
 
-int Trie_delete_node(TrieNode** node) {
-    if (!node || !*node || !(*node)->end_of_word) {
+int List_insert(Node** head, char* string, int length) {
+    if (head == NULL || string == NULL) {
+        return 2;
+    }
+    if (*head && List_search(*head, string) != NULL) {
+        return 2;
+    }
+    Node* new_node = malloc(sizeof(Node));
+    if (!new_node) {
+        return 1;
+    }
+    new_node->string = calloc(length + 1, sizeof(char));
+    new_node->length = length;
+    int i;
+    for (i = 0; i < length + 1; i++) {
+        new_node->string[i] = string[i];
+    }
+    new_node->next = NULL;
+    if (*head == NULL) {
+        *head = new_node;
         return 0;
     }
-    (*node)->end_of_word = 0;
+    new_node->next = *head;
+    *head = new_node;
+    return 0;
+}
+
+
+int List_delete(Node** head, char* string) {
+    if (head == NULL) {
+        return 0;
+    }
+    Node* parent = NULL;
+    Node* current = *head;
+    while (current && !string_equal(current->string, string)) {
+        parent = current;
+        current = current->next;
+    }
+    if (!current) {
+        return 0;
+    }
+    if (parent) {
+        parent->next = current->next;
+    } else {
+        *head = current->next;
+    }
+    free(current->string);
+    free(current);
     return 1;
 }
 
 
-TrieNode* Trie_new_node(void) {
-    TrieNode* new_node = calloc(1, sizeof(TrieNode));
-    new_node->end_of_word = 0;
-    return new_node;
+void List_free(Node **head) {
+    if (head == NULL || *head == NULL) {
+        return;
+    }
+    Node *current = *head;
+    Node *next;
+    while (current) {
+        next = current->next;
+        free(current->string);
+        free(current);
+        current = next;
+    }
+    *head = NULL;
 }
 
 
-TrieNode* Trie_push(TrieNode* root, char* word, int length) {
-    if (root == NULL || word == NULL) {
-        root = Trie_new_node();
-    }
-    TrieNode* current = root;
+typedef struct Hash_table {
+    int max_size;
+    Node** values;
+} Hash_table;
+
+
+double fraction(double a) {
+    return a - (int)a;
+}
+
+
+int Hash_function(char* string, int length) {
+    int x = 6;
+    int hash = 1;
     int i;
-    int index;
     for (i = 0; i < length; i++) {
-        index = get_index(word[i]);
-        if (!current->children[index]) {
-            current->children[index] = Trie_new_node();
-        }
-        current = current->children[index];
+        hash += get_index(string[i]) * hash * x;
     }
-    current->end_of_word = 1;
-    return root;
+    return (int)(fraction(hash * A) * TABLE_SIZE) % TABLE_SIZE;
 }
 
 
-int Trie_free_node(TrieNode** node) {
-    if (node == NULL || *node == NULL) {
-        return 0;
-    }
+int Table_push(Hash_table* hash_table, char* string, int length) {
+    return List_insert(&hash_table->values[Hash_function(string, length)], string, length);
+}
+
+
+int Table_delete_value(Hash_table* hash_table, char* string, int length) {
+    return List_delete(&hash_table->values[Hash_function(string, length)], string);
+}
+
+
+int Table_search(Hash_table* hash_table, char* string, int length) {
+    Node* found = List_search(hash_table->values[Hash_function(string, length)], string);
+    return found != NULL;
+}
+
+
+int Table_clear(Hash_table* hash_table) {
     int i;
-    for (i = 0; i < ALPHABET_SIZE; i++) {
-        if ((*node)->children[i]) {
-            Trie_free_node(&(*node)->children[i]);
-        }
+    for (i = 0; i < TABLE_SIZE; i++) {
+        List_free(&hash_table->values[i]);
     }
-    free(*node);
-    *node = NULL;
-    return 1;
+    return 0;
 }
 
 
@@ -93,7 +171,7 @@ int read_word(int* length, char* string) {
         return 0;
     }
     *length = 0;
-    scanf(" %s", string + *length);
+    scanf(" %c", string + *length);
     (*length)++;
     while (scanf("%c", string + *length) != EOF && string[*length] != '\n') {
         (*length)++;
@@ -104,21 +182,21 @@ int read_word(int* length, char* string) {
 
 int main(void) {
     char command;
-    TrieNode* root = NULL;
+    Hash_table* hash_table = malloc(sizeof(Hash_table));
+    hash_table->max_size = TABLE_SIZE;
+    hash_table->values = calloc(hash_table->max_size, sizeof(Node*));
     int length = 0;
-    char string[MAX_LENGTH];
-    TrieNode** found;
+    char* string = calloc(MAX_LENGTH, sizeof(char));
     do {
         scanf(" %c", &command);
         switch (command) {
             case '+':
                 read_word(&length, string);
-                root = Trie_push(root, string, length);
+                Table_push(hash_table, string, length);
                 break;
             case '?':
                 read_word(&length, string);
-                found = Trie_search_node(&root, string, length);
-                if (found && *found && (*found)->end_of_word) {
+                if (Table_search(hash_table, string, length)) {
                     printf("YES\n");
                 } else {
                     printf("NO\n");
@@ -126,15 +204,14 @@ int main(void) {
                 break;
             case '-':
                 read_word(&length, string);
-                found = Trie_search_node(&root, string, length);
-                if (found && *found) {
-                    Trie_delete_node(found);
-                }
+                Table_delete_value(hash_table, string, length);
                 break;
             default:
                 break;
         }
     } while (command != '#');
-    Trie_free_node(&root);
+    Table_clear(hash_table);
+    free(hash_table->values);
+    free(hash_table);
     return 0;
 }
